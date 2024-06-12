e70566a0264f7ebb6ba98ac228bc2caa6b2fd8ed
chepykurniawan <113526589+ChepyKurniawan@users.noreply.github.com>
2024-06-12 19:46:37 +0700

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Ananda Chepy Kurniawan</span> Saya adalah seorang desainer website dan UI/UX."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
+
+
